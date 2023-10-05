340b0348447f989641c37e1dd3b09f668352cdca
Rhabi <rhabidpmendoza@gmail.com>
2023-10-05 16:42:27 +0800

    Create LengthOfLastWord.java

---
+++ LengthOfLastWord.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        // Start of Solution
+        String words[] = s.split(" ");
+        String word = words[words.length - 1];
+
+        return word.length();
+
+        // End of Solution
+
+    }
+}
